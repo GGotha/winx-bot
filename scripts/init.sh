eadcab0ebdd8f1e827afb8e0697613af95b257c1
GGotha <snakygamer@gmail.com>
2021-08-06 22:47:00 -0300

    scripts workings

--- scripts/init.sh
+++ scripts/init.sh
@@ -2,3 +2,4 @@ cd ~/ &&
 apt-get update &&
 snap install docker &&
 docker run --name winxbot --env-file ~/env/.env.production -d gothathemyth/winx-bot:latest
+docker-compose -f ~/env/docker-compose.yml up
